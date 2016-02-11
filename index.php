044d3021bf7a2d5e9792292f705e2cc3196b3738
WillyWonka125 <william.m.erickson@gmail.com>
2016-02-11 17:18:00 -0600

    Change to php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <div class="container">
+      <h1>WILL ERICKSON's GENETICS PROGRAM </h1>
+    </div>
+    
+    <div class="container>
+      <p>Type in nucleotide sequence</p>
+      
+    </div>
+    
+  </body>
+</html>
